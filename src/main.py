208aeffea674dbd643d9f7e8d27197a8dbc23cd9
pablo <pablodhe@ext.inditex.com>
2023-10-01 15:47:32 +0200

    Added new data types and startup db fill

--- src/main.py
+++ src/main.py
@@ -1,11 +1,33 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.exceptions import ValidationException
 from fastapi.responses import JSONResponse
 
+from model.dto.order_field import OrderField
+from model.enums.order_field_type import OrderFieldType
+from model.requests.product_request import ProductRequest
 from repositories.order_repository import OrderRepository
 from routes import product_router, order_router
+from services.product_service import ProductService
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    ProductService().create_new(
+        ProductRequest(name="Surf", description="Clases de surf en Bastiagueiro", price=25,
+                       order_fields=[OrderField(name="date", type=OrderFieldType.Date),
+                                     OrderField(name="time", type=OrderFieldType.Time),
+                                     OrderField(name="additional_notes",
+                                                type=OrderFieldType.String)]))
+    ProductService().create_new(
+        ProductRequest(name="Desayuno", description="Desyuno en el hotel Pueblo Paleta", price=10,
+                       order_fields=[OrderField(name="date", type=OrderFieldType.Date),
+                                     OrderField(name="breakfast_type", type=OrderFieldType.String)]))
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 order_repository = OrderRepository()
 
--- src/model/enums/order_field_type.py
+++ src/model/enums/order_field_type.py
@@ -4,15 +4,30 @@ from time import strptime
 
 class OrderFieldType(Enum):
     Date = "date"
+    Time = "time"
     String = "string"
+    Integer = "int"
+    Float = "float"
 
     def validate(self, value: any):
-        def is_datetime(x):
+        def is_date(x):
             try:
                 strptime(x, '%d-%m-%Y')
                 return True
             except:
                 return False
 
-        validators = {OrderFieldType.Date: is_datetime, OrderFieldType.String: lambda x: type(x) == str}
+        def is_time(x):
+            try:
+                strptime(x, "%H:%M:%S")
+                return True
+            except:
+                return False
+
+        validators = {OrderFieldType.Date: is_date,
+                      OrderFieldType.Time: is_time,
+                      OrderFieldType.String: lambda x: isinstance(x, str),
+                      OrderFieldType.Integer: lambda x: isinstance(x, int),
+                      OrderFieldType.Float: lambda x: isinstance(x, float)}
+
         return validators[self](value)
